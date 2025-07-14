a87db1ac97dcb52f2a63bc8fac217fa836802634
Volodymyr Havrylov <128315440+havrylovv@users.noreply.github.com>
2025-07-15 01:17:02 +0200

    fix: add image normalization to config

--- configs/mono_detr3d.py
+++ configs/mono_detr3d.py
@@ -38,6 +38,7 @@ train_transforms = dict(
                 [transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1)], p=0.3
             ),
             transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     ),
     pc=transforms.Compose(
